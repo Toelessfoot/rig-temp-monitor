675183c50c4df5e421b66c4e4b4ad21c2f7e423f
MG <toelessfoot@gmail.com>
2023-08-02 17:29:31 -0400

    modified add-cron

--- add-cron.sh
+++ add-cron.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+curr_date=$(date +%m%d%H%M)
+
 crontab -l > mycron
+crontab -l > /cronBackups/mycronBACKUP$curr_date
 echo "@reboot sleep 120 && python3 /rig-temp-monitor/rig-temp-monitor.py" >> mycron
 crontab mycron
 rm mycron
