0c8a2270a09b15ae0c7fae16da92e2a70ccc3080
MG <toelessfoot@gmail.com>
2023-08-02 16:24:00 -0400

    ready for deployment

--- rig-temp-monitor.py
+++ rig-temp-monitor.py
@@ -3,6 +3,23 @@ import subprocess
 import re
 from sys import argv
 import os
+import requests
+from random import randrange
+
+minute = 60
+hour = minute * 60
+timeout = 1 * hour + randrange(1 * hour)
+timeout_min = timeout / 60
+
+def send_telegram_msg(msg):
+    print(msg)
+    data = {
+        'text': msg,
+        'chat_id': '-795504579'
+    }
+    req = requests.post(tele_url, data=data)
+    print(req)
+    print(req.text)
 
 #get script directory and change cwd to it
 print(f'Current Working Path: {os.getcwd()}')
@@ -10,28 +27,43 @@ script_dir = os.path.dirname(os.path.abspath(argv[0]))
 print(f'Changing cwd to: {script_dir}')
 os.chdir(script_dir)
 
+hostname = subprocess.check_output("hostname", shell=True, encoding='utf-8').strip()
+print(hostname)
+
+#get Telegram key
+with open("tele.txt", "r") as f:
+    token = f.read().strip()
+
+tele_url = f'https://api.telegram.org/bot{token}/sendMessage'
+print(tele_url)
+
 while True:
+    #temperature limit
     with open("limit.txt", "r") as f:
         limit = int(f.read())
 
     print(f'Limit is: {str(limit)}')
 
-    with open("toggle.txt", "r") as f:                                                                                                     
-        toggle = f.read()                                                                                                              
-        toggle = toggle.strip()                                                                                                        
-        print(f'Toggle is: {toggle}')                                                                                                                  
-        if toggle == "on":                                                                                                             
-            print("LIMIT ON")                                                                                                      
-                                                                                                                                           
-            #out = subprocess.check_output("nvidia-smi", shell=True, encoding='utf-8')                                                      
+    #toggle in file turns on or off
+    with open("toggle.txt", "r") as f:
+        toggle = f.read()
+        toggle = toggle.strip()
+        print(f'Toggle is: {toggle}')
+        if toggle == "on":
+            print("LIMIT ON")
+
+            #out = subprocess.check_output("nvidia-smi", shell=True, encoding='utf-8')
             with open("nvidia-smi.txt", "r") as f:
                 out = f.read()
-                                                                                                                                           
-            reg = re.findall("\d{2,3}(?=C)", out)                                                                                          
-                                                                                                                                           
-            for i in reg:                                                                                                                  
-                print(i)                                                                                                               
-                if int(i) > limit:                                                                                                        
-                    print("HIGH")                                                                                                  
-        sleep(30)                                                                                                                      
-                                 
+
+            reg = re.findall("\d{2,3}(?=C)", out)
+
+            for i in reg:
+                print(i)
+                if int(i) > limit:
+                    print("HIGH")
+                    send_telegram_msg(f"{hostname} - HIGH TEMPS shutting down for {timeout_min} min")
+                    subprocess.run(f"sreboot wakealarm {timeout}")
+                    exit()
+    sleep(30)
+
