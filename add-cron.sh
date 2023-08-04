5a12feddb63920e2e8f08cf4fb9bad0e49a5a732
MG <toelessfoot@gmail.com>
2023-08-04 12:29:52 -0400

    changed add cron

--- add-cron.sh
+++ add-cron.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-crontab=$(crontab -l -u user)
-search="rig-temp-monitor"
-
-if [[ "$crontab" == *"$search"* ]]; then
-    echo "It's there."
-else
-    echo "It's not there. Adding it to cron"
-    curr_date=$(date +%m%d%H%M)
-    crontab -l -u user > mycron
-    crontab -l -u user > mycronBACKUP$curr_date
-    echo "@reboot python3 /rig-temp-monitor/rig-temp-monitor.py" >> mycron
-    crontab -u user mycron
-    rm mycron
-fi
+curr_date=$(date +%m%d%H%M)
+crontab -l -u user > mycronBACKUP$curr_date
+echo "@reboot python3 /rig-temp-monitor/rig-temp-monitor.py" > mycron
+crontab -u user mycron
+rm mycron
