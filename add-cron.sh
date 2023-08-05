37521709d2471f6c196a293dd8549646f2e1b334
MG <toelessfoot@gmail.com>
2023-08-05 10:24:43 -0400

    back to user dir

--- add-cron.sh
+++ add-cron.sh
@@ -2,6 +2,6 @@
 
 curr_date=$(date +%m%d%H%M)
 crontab -l -u user > mycronBACKUP$curr_date
-echo "@reboot python3 /rig-temp-monitor/rig-temp-monitor.py >> rig-temp-monitor.log" > mycron
+echo "@reboot python3 /home/user/rig-temp-monitor/rig-temp-monitor.py >> /home/user/rig-temp-monitor.log 2>&1" > mycron
 crontab -u user mycron
 rm mycron
