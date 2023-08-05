195ca4d63d5529ad1026225b37f375bc158b32bd
MG <toelessfoot@gmail.com>
2023-08-05 09:53:58 -0400

    added logging to cron job

--- add-cron.sh
+++ add-cron.sh
@@ -2,6 +2,6 @@
 
 curr_date=$(date +%m%d%H%M)
 crontab -l -u user > mycronBACKUP$curr_date
-echo "@reboot python3 /rig-temp-monitor/rig-temp-monitor.py" > mycron
+echo "@reboot python3 /rig-temp-monitor/rig-temp-monitor.py >> rig-temp-monitor.log" > mycron
 crontab -u user mycron
 rm mycron
